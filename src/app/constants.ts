0573e29833af200b96c80486e1439b4c2be697de
Muthaiyyan <ephrash@yahoo.com>
2025-03-12 14:22:33 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://af7d0831f3c1542568120501f16c33f1-303019523.us-east-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://af7d0831f3c1542568120501f16c33f1-303019523.us-east-1.elb.amazonaws.com/api/',
 };
